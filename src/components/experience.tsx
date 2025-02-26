fea250e58b0c081c05e9660863e1b153d766807a
zakaria <zouali1@toromail.csudh.edu>
2025-02-26 12:09:27 -0800

    initial commit

---
+++ src/components/experience.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Experience() {
+  return (
+    <div className="p-1">
+      <h5 className="mb-2 text-m font-bold tracking-tight">Experience</h5>
+      <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
+      <div className="mb-3 font-normal">
+        <strong>California State University, Dominguez Hills</strong>
+        <p className="text-sm font-light">Master of Science, Computer Science</p>
+        <p className="text-sm font-light italic">August 2024 - May 2026</p>
+        <p className="text-sm font-light">
+          At California State University, Dominguez Hills, I am pursuing a Master of Science degree
+        </p>
+      </div>
+    </div>
+  );
+}
